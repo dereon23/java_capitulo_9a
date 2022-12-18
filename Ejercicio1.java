49c06b33fef578fdb5433f470274a700fe55619a
dereon23 <115071403+dereon23@users.noreply.github.com>
2022-12-18 23:39:59 +0100

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,9 @@
+package Capitulo9a;
+
+public class Ejercicio1 {
+
+	//Atributos: altura, victorias, velocidadMaximaAlcanzada
+	//FernandoAlonso: 1.71 m, 32, 342
+	
+	
+}
---
+++ Ejercicio2.java
@@ -0,0 +1,40 @@
+package Capitulo9a;
+
+public class Ejercicio2 {
+	
+	/*
+	 * Clases:
+	 * 
+	 * perro: hijo de animal
+	 * mineral
+	 * caballo: hijo de animal
+	 * gato: hijo de animal
+	 * animal
+	 * bucefalo: hijo de animal
+	 * pegaso: hijo de caballo
+	 * ayudante_de_santa_claus: hijo de persona
+	 * persona
+	 * 
+	 * */
+	
+	/*
+	 * Instancias:
+	 * 
+	 * paula: persona
+	 * goofy: perro
+	 * gardfiel: gato
+	 * tom: gato
+	 * silvestre: gato
+	 * rocinante: caballo
+	 * milu: perro
+	 * snoopy: perro
+	 * pluto: perro
+	 * pirita: mineral
+	 * javier: persona
+	 * cuarzo: mineral
+	 * laika: perro
+	 * pato_lucas: animal
+	 * 
+	 * */
+
+}
